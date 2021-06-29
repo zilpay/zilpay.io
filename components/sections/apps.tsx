2a68b68d8713269db9338a7e268a7bce49b3adca
Rinat <lich666black@gmail.com>
2021-06-29 18:21:01 +0500

    added apps section

--- components/sections/apps.tsx
+++ components/sections/apps.tsx
@@ -1,43 +1,27 @@
 import React from 'react';
-import styled from 'styled-components';
+import { useRouter } from 'next/router';
 
-import { Card } from 'components/card';
 import { Text } from 'components/text';
+import { ZoomCard } from 'components/zoom-card';
+
 import { StyleFonts } from '@/config/fonts';
 import { Colors } from '@/config/colors';
 
-const Container = styled.section`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-
-  width: 100%;
-  height: 100%;
-
-  margin-bottom: 20vh;
-  margin-top: 20vh;
-`;
-const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-wrap: wrap;
-
-  max-width: 1200px;
-  width: 100%;
-  height: 100%;
-
-  @media (max-width: 1000px) {
-    flex-direction: column;
-    flex-wrap: nowrap;
-  }
-`;
-const HeaderWrapper = styled.div`
-  width: 100%;
-  margin-left: 22vw;
-`;
+import { Container, Wrapper, HeaderWrapper } from './styles';
 
 export const AppsSection: React.FC = () => {
+  const router = useRouter();
+
+  const hanldeClick = React.useCallback((url: string, native: boolean) => {
+    if (native) {
+      router.push(url);
+    }
+
+    if (!native && process.browser) {
+      window.open(url, '_blank');
+    }
+  }, [router]);
+
   return (
     <Container>
       <HeaderWrapper>
@@ -57,6 +41,54 @@ export const AppsSection: React.FC = () => {
         </Text>
       </HeaderWrapper>
       <Wrapper>
+        <ZoomCard
+          url="https://zilpay.io/wp-content/uploads/2021/03/vlx-konstantinov-02-1-1024x576.png"
+          href="https://dragonzil.xyz/"
+          title="DragonZIL"
+          onClick={(url) => hanldeClick(url, false)}
+        >
+          Buy and train dragons
+        </ZoomCard>
+        <ZoomCard
+          url="https://zilpay.io/wp-content/uploads/2021/03/kyle-brinker-0vVHYD3PcKo-unsplash_3-1-1024x822.jpg"
+          href="https://unstoppabledomains.com/"
+          title="UnstoppableDomains"
+          onClick={(url) => hanldeClick(url, false)}
+        >
+          buy and manage domains
+        </ZoomCard>
+        <ZoomCard
+          url="https://zilpay.io/wp-content/uploads/2021/03/fingerprint-1024x683.png"
+          href="https://zilpay-b255e.web.app/Verification"
+          title="Signature Verification"
+          onClick={(url) => hanldeClick(url, false)}
+        >
+          Verify transactions signatures
+        </ZoomCard>
+        <ZoomCard
+          url="https://zilpay.io/wp-content/uploads/2021/03/UD-laptop-1024x683.png"
+          href="https://stake.zilliqa.com/"
+          title="Stake"
+          onClick={(url) => hanldeClick(url, false)}
+        >
+          Delegate validators
+        </ZoomCard>
+        <ZoomCard
+          url="https://zilpay.io/wp-content/uploads/2021/03/data-1024x682.png"
+          href="https://ide.zilliqa.com/#/"
+          title="Scilla Editor"
+          onClick={(url) => hanldeClick(url, false)}
+        >
+          Edit and deploy a smart contract on scilla language
+        </ZoomCard>
+        <ZoomCard
+          url="https://zilpay.io/wp-content/uploads/2021/03/graph-1024x682.png"
+          href="https://zilswap.io/swap"
+          title="Zilswap"
+          onClick={(url) => hanldeClick(url, false)}
+        >
+          Swap or provide liquidity to ZRC2 tokens
+        </ZoomCard>
       </Wrapper>
     </Container>
   );
--- components/zoom-card.tsx
+++ components/zoom-card.tsx
@@ -1,24 +1,82 @@
 import React from 'react';
 import styled from 'styled-components';
-import Link from 'next/link';
 
 import { Text } from 'components/text';
-import { Button } from 'components/button';
 
 import { Colors } from '@/config/colors';
 import { StyleFonts } from '@/config/fonts';
 
+type ContainerProp = {
+  image: string;
+};
+
 const Container = styled.div`
+  cursor: pointer;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+
+  width: 290px;
+  height: 290px;
+
+  margin: 16px;
+
+  float: left;
+  overflow: hidden;
+  transition: all 0.4s;
+
+  background-color: ${Colors.Dark};
+  background-image: url(${(p: ContainerProp) => p.image});
+  background-size: cover;
+  background-position: 50%;
+
+  :hover {
+    border-color: ${Colors.Secondary};
+    background-size: 700px;
+    background-position: 40%;
+  }
+
+  @media (max-width: 1000px) {
+    width: 80vw;
+  }
 `;
 
 type Prop = {
   url: string;
+  href: string;
   title: string;
+  onClick?: (url: string) => void;
 };
 
-export const ZoomCard: React.FC<Prop> = ({ url, title, children }) => {
+export const ZoomCard: React.FC<Prop> = ({
+  url,
+  title,
+  href,
+  children,
+  onClick = () => null
+}) => {
   return (
-    <Container>
+    <Container
+      image={url}
+      onClick={() => onClick(href)}
+    >
+      <Text
+        fontColors={Colors.White}
+        fontVariant={StyleFonts.Bold}
+        size="24px"
+      >
+        {title}
+      </Text>
+      <Text
+        fontColors={Colors.White}
+        fontVariant={StyleFonts.SemiBold}
+        size="14px"
+      >
+        {children}
+      </Text>
     </Container>
   );
 };
