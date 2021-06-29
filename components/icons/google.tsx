2251661c5edde4c932d89f41d6bd5ab3c7bcde09
Rinat <lich666black@gmail.com>
2021-06-29 21:39:55 +0500

    fixed icons

--- components/icons/google.tsx
+++ components/icons/google.tsx
@@ -6,17 +6,26 @@ type Prop = {
   width: number | string;
   height: number | string;
   color: Colors;
+  hoverColor?: Colors;
 };
 
-export const GoogleIcon: React.FC<Prop> = ({ width, height, color }) => {
+export const GoogleIcon: React.FC<Prop> = ({
+  width,
+  height,
+  color,
+  hoverColor = Colors.White
+}) => {
+  const [hoverd, setHover] = React.useState(false);
   return (
     <svg
       width={width}
       height={height}
       viewBox="0 0 24 24"
       fill="none"
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
     >
-      <path d="M3 22V2L21 12L3 22Z" fill={color}/>
+      <path d="M3 22V2L21 12L3 22Z" fill={hoverd ? hoverColor : color}/>
     </svg>
   );
 };
