1e916d02ecae5d0e3bce033883a0d56dfc39259a
Rinat <lich666black@gmail.com>
2022-01-18 13:18:40 +0300

    fixed header and install button

--- components/Install-button.tsx
+++ components/Install-button.tsx
@@ -20,7 +20,29 @@ const GetButton = styled(Button)`
 export const InstallButton: React.FC = () => {
   const { t } = useTranslation(`main`);
 
-  if (isChrome) {
+ if (isAndroid) {
+    return (
+      <a href="https://play.google.com/store/apps/details?id=com.zilpaymobile">
+        <GetButton
+          color={Colors.Secondary}
+          fontColors={Colors.Secondary}
+        >
+          {t(`get_android`)}
+        </GetButton>
+      </a>
+    );
+  } if (isIOS) {
+    return (
+      <a href="https://apps.apple.com/ru/app/zilpay/id1547105860">
+        <GetButton
+          color="#bfe2e8"
+          fontColors="#bfe2e8"
+        >
+          {t(`get_ios`)}
+        </GetButton>
+      </a>
+    );
+  } if (isChrome) {
     return (
       <a href="https://chrome.google.com/webstore/detail/zilpay/klnaejjgbibmhlephnhpmaofohgkpgkd">
         <GetButton
@@ -50,28 +72,6 @@ export const InstallButton: React.FC = () => {
           fontColors="#CC2993"
         >
           {t(`get_firefox`)}
-        </GetButton>
-      </a>
-    );
-  } if (isAndroid) {
-    return (
-      <a href="https://play.google.com/store/apps/details?id=com.zilpaymobile">
-        <GetButton
-          color={Colors.Secondary}
-          fontColors={Colors.Secondary}
-        >
-          {t(`get_android`)}
-        </GetButton>
-      </a>
-    );
-  } if (isIOS) {
-    return (
-      <a href="https://apps.apple.com/ru/app/zilpay/id1547105860">
-        <GetButton
-          color="#bfe2e8"
-          fontColors="#bfe2e8"
-        >
-          {t(`get_ios`)}
         </GetButton>
       </a>
     );
--- next.config.js
+++ next.config.js
@@ -1,6 +1,38 @@
 const { i18n } = require('./next-i18next.config');
 
+const securityHeaders = [
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  }
+];
+
 module.exports = {
+  i18n,
   reactStrictMode: true,
-  i18n
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
 }
