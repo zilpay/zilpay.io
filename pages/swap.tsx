7735b5faf007c3a219d542aeb23fa54823ce4ac5
Rinat <lich666black@gmail.com>
2022-04-01 19:48:12 +0300

    generated swap page

---
+++ pages/swap.tsx
@@ -0,0 +1,37 @@
+import styles from '@/styles/pages/swap.module.scss';
+
+import { useTranslation } from 'next-i18next';
+import Head from 'next/head';
+import React from 'react';
+import { GetServerSidePropsContext, NextPage } from 'next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
+// import { SwapForm } from '@/components/dex/swap';
+
+
+export const PageSwap: NextPage = () => {
+  const { t } = useTranslation(`main`);
+  return (
+    <div className={styles.container}>
+       <Head>
+        <title>Swap</title>
+        <meta
+          property="og:title"
+          content={'Swap'}
+          key="title"
+        />
+      </Head>
+      <div>
+        {/* <SwapForm /> */}
+      </div>
+    </div>
+  );
+}
+
+export const getStaticProps = async (props: GetServerSidePropsContext) => ({
+  props: {
+    ...await serverSideTranslations(props.locale || `en`, [`swap`, `common`]),
+  },
+});
+
+export default PageSwap;
---
+++ types/token.d.ts
@@ -0,0 +1,7 @@
+export interface TokenState {
+  decimals: number;
+  bech32: string;
+  base16: string;
+  name: string;
+  symbol: string;
+}
