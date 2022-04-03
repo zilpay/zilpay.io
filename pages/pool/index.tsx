f1070765977e7f634a8aa8f52ceedda68a003c22
Rinat <lich666black@gmail.com>
2022-04-03 13:45:10 +0300

    added add pool styles

--- pages/pool/index.tsx
+++ pages/pool/index.tsx
@@ -1,14 +1,29 @@
 import styles from '@/styles/pages/swap.module.scss';
 
 import Head from 'next/head';
+import { useStore } from 'react-stores';
 import React from 'react';
 import { GetServerSidePropsContext, NextPage } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import { PoolOverview } from '@/components/pool';
+import { DragonDex } from '@/mixins/dex';
+import { $wallet } from '@/store/wallet';
 
-
+const dex = new DragonDex();
 export const PagePool: NextPage = () => {
+  const wallet = useStore($wallet);
+
+  const hanldeUpdate = React.useCallback(async() => {
+    if (wallet) {
+      await dex.updateState(wallet.base16);
+    }
+  }, [wallet]);
+
+  React.useEffect(() => {
+    hanldeUpdate();
+  }, [wallet, hanldeUpdate]);
+
   return (
     <div className={styles.container}>
       <Head>
