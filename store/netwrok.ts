578983594092d1cc64e0e8fd9790a5bdcd270770
Rinat <lich666black@gmail.com>
2022-05-17 16:08:54 +0300

    fixed netwrok store

--- store/netwrok.ts
+++ store/netwrok.ts
@@ -2,7 +2,7 @@ import { NET } from '@/config/conts';
 import { Store } from 'react-stores';
 
 const init: {
-  net: 'mainnet' | 'testnet' | 'private';
+  net: 'mainnet' | 'testnet' | 'private' | string;
 } = {
   net: NET
 }
