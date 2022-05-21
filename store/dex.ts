1a291b4b1877f049364a183d1ac793645b0d6469
Rinat <lich666black@gmail.com>
2022-05-21 13:21:06 +0300

    fixed fee

--- store/dex.ts
+++ store/dex.ts
@@ -2,7 +2,7 @@ import { Store } from 'react-stores';
 
 const init = {
   lp: BigInt(100000000000000),
-  fee: BigInt(10000),
+  fee: BigInt(9950),
   protoFee: BigInt(500)
 }
 export const $dex = new Store(init);
