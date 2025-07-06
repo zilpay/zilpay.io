851f5c9af53394b47728a0c8674dae46233a6128
Rinat <rinat@Rinats-MacBook-Air.local>
2025-07-07 00:38:11 +0900

    fixed headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 const { i18n } = require('./next-i18next.config');
 
 const securityHeaders = [
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin'
+  }
 ];
 
 /** @type {import('next').NextConfig} */
@@ -12,15 +32,15 @@ const nextConfig = {
     disableStaticImages: true,
     dangerouslyAllowSVG: true
   },
-  // headers() {
-  //   return [
-  //     {
-  //       // Apply these headers to all routes in your application.
-  //       source: '/:path*',
-  //       headers: securityHeaders,
-  //     },
-  //   ]
-  // }
+  headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
